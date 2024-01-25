d2fef87fde9328107578c02cea8d62bbbe11edc2
michelmnds <m1tchofc@gmail.com>
2024-01-25 12:03:54 +0000

    feat: auth routes done

---
+++ routes/auth.routes.js
@@ -0,0 +1,93 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/User.model.js");
+const bcrypt = require("bcrypt");
+const { isAuth } = require("../middleware/authentication.middleware.js");
+
+const router = require("express").Router();
+
+router.post("/signup", async (req, res) => {
+  const { email, username, password } = req.body;
+
+  try {
+    const potentialUser = await User.findOne({ email });
+
+    if (!potentialUser) {
+      const salt = bcrypt.genSaltSync(13);
+      const passwordHash = bcrypt.hashSync(password, salt);
+
+      try {
+        const newUser = await User.create({ email, username, passwordHash });
+
+        res.status(201).json(newUser);
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      res.status(400).json({ error: "User already exists" });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+router.post("/login", async (req, res) => {
+  const { loginCredential, password } = req.body;
+
+  if (loginCredential.split("").includes("@")) {
+    const email = loginCredential;
+
+    try {
+      const potentialUser = await User.findOne({ email });
+
+      if (potentialUser) {
+        const passwordCorrect = bcrypt.compareSync(
+          password,
+          potentialUser.passwordHash
+        );
+        if (passwordCorrect) {
+          const authToken = jwt.sign(
+            { userId: potentialUser._id },
+            process.env.TOKEN_SECRET,
+            { algorithm: "HS256", expiresIn: "12h" }
+          );
+          res.status(200).json({ token: authToken });
+        } else {
+          res.status(403).json({ message: "Invalid email or password" });
+        }
+      } else {
+        res.status(401).json({ error: "Invalid email or password" });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+    const username = loginCredential;
+
+    try {
+      const potentialUser = await User.findOne({ username });
+
+      if (potentialUser) {
+        const passwordCorrect = bcrypt.compareSync(password);
+        if (passwordCorrect) {
+          const authToken = jwt.sign(
+            { userId: potentialUser._id },
+            process.env.TOKEN_SECRET,
+            { algorithm: "HS256", expiresIn: "12h" }
+          );
+          res.status(200).json({ token: authToken });
+        } else {
+          res.status(403).json({ message: "Invalid username or password" });
+        }
+      } else {
+        res.status(401).json({ error: "Invalid username or password" });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+});
+router.get("/verify", isAuth, async (req, res) => {
+  const currentUser = await User.findById(req.tokenPayload.userId);
+  res.status(200).json(currentUser);
+});
+
+module.exports = router;
