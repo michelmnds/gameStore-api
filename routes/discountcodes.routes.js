be4bbfd6d12e1aa1bfe7b70ee1e0d9618c17770e
Tom Lorius <tomlorius.mail@gmail.com>
2024-01-28 16:59:50 +0100

    added discount code routes

--- routes/discountcodes.routes.js
+++ routes/discountcodes.routes.js
@@ -4,7 +4,7 @@ const router = require("express").Router();
 
 //create new discount code - only admins should be allowed to do that
 
-router.post("/", async (req, res, next) => {
+router.post("/", isAuth, async (req, res, next) => {
   const { code, discountInPercent, appliesToAlreadyDiscoutedGames } = req.body;
   try {
     const createdCode = await Discountcode.create({
@@ -19,5 +19,28 @@ router.post("/", async (req, res, next) => {
 });
 
 //GET to get information about a discount code that was applied in checkout
+router.get("/:discountcode", async (req, res, next) => {
+  const { discountcode } = req.params;
+  try {
+    const discCode = await Discountcode.findOne({ code: discountcode });
+    res.status(200).json(discCode);
+  } catch (error) {
+    next(error);
+  }
+});
+
+//PUT Edit an existing code - should also be admin only - just make sure the correct info is passed
+router.put("/:codeId", isAuth, async (req, res, next) => {
+  const { codeId } = req.params;
+  const payload = req.body;
+  try {
+    const updatedCode = await Discountcode.findByIdAndUpdate(codeId, payload, {
+      new: true,
+    });
+    res.status(200).json(updatedCode);
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
