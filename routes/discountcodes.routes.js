345b62c9ea1d6866ce1db43049baae17c5118546
Tom Lorius <tomlorius.mail@gmail.com>
2024-01-30 10:48:11 +0100

    added 2 discountcode routes

--- routes/discountcodes.routes.js
+++ routes/discountcodes.routes.js
@@ -5,6 +5,16 @@ const {
 } = require("../middleware/authentication.middleware.js");
 const router = require("express").Router();
 
+//get all discount codes - only admins can do that
+router.get("/", isAuth, isAdmin, async (req, res, next) => {
+  try {
+    const allCodes = await Discountcode.find();
+    res.status(200).json(allCodes);
+  } catch (error) {
+    next(error);
+  }
+});
+
 //create new discount code - only admins should be allowed to do that
 
 router.post("/", isAuth, isAdmin, async (req, res, next) => {
